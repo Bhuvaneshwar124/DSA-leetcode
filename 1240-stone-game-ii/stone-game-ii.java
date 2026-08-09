6aaec61957b1b0d16423c5dfd722ea0b338659c7
Bhuvaneshwar S <bhuvaneshwars12@gmail.com>
2026-08-09 06:43:47 +0530

    Time: 5 ms (71.42%) | Memory: 43.6 MB (98.99%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[] psum;
+    private int solve(int index, int m, int[] piles, int[][] dp) {
+        if (index >= piles.length) return 0;
+        if (dp[index][m] != -1) return dp[index][m];
+
+        int total = psum[piles.length] - psum[index];
+        int ans = 0;
+        for(int x=1;x<=Math.min(2*m,piles.length-index);x++) {
+            int newM = Math.max(m,x);
+            ans = Math.max(ans,total-solve(index + x, newM, piles, dp));
+        }
+
+        return dp[index][m] = ans;
+    }
+    public int stoneGameII(int[] piles) {
+        int n=piles.length;
+        psum=new int[n+1];
+        for(int i=0;i<n;i++){
+           psum[i+1]=piles[i]+psum[i];
+        }
+        int[][] dp=new int[n][n+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(0,1,piles,dp);
+    }
+}
